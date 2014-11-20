c7bdf960b11aae00b4af64a06808c80bb5b8a6e4
GeoffPurdy <geoff_purdy@yahoo.com>
2014-11-20 18:07:01 -0500

    added webview pane for defiinitions

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,13 @@
 function doClick(e) {
-    alert($.label.text);
+  var url = "http://api.openweathermap.org/data/2.5/weather?q=Durham,nc";
+  var json;
+ 
+  var xhr = Ti.Network.createHTTPClient({
+    onload: function() {
+    // parse the retrieved data, turning it into a JavaScript object
+    json = JSON.parse(this.responseText);
+	}
+  });
 }
 
 $.index.open();
