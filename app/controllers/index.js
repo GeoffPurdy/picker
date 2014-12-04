63518ee41ddbad0dddc97c1c84529626e0227885
GeoffPurdy <geoff_purdy@yahoo.com>
2014-12-04 15:41:41 -0500

    getting path via JSON and Rails

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,12 +1,14 @@
 
 
-var word = "FOOL".split("");
+var word = "WORD".split("");
+var golden_path = [];
 var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
 var selected;
 
 
 
-for(var j=0;j<word.length;j++) { // FIXME words greater than 13-14 letters drop the last picker column
+function initPickerColumns() {
+  for(var j=0;j<word.length;j++) { // FIXME words greater than 13-14 letters drop the last picker column
     var new_column = Ti.UI.createPickerColumn();
 	for (var i = 0; i < alphabet.length; i++) {
 		var row = Ti.UI.createPickerRow();
@@ -14,6 +16,19 @@ for(var j=0;j<word.length;j++) { // FIXME words greater than 13-14 letters drop 
 		new_column.addRow(row);
 	}
     $.picker.add(new_column);
+  }
+}
+
+function setPickerRows() {
+  Ti.API.info("in open event listener.");
+  for(var k=0; k<word.length; k++) {
+    Ti.API.info("word[k]=" + word[k]);
+    Ti.API.info("indexOf()=" + alphabet.indexOf( word[k] ));
+    $.picker.setSelectedRow(k, alphabet.indexOf( word[k] ), false);
+  }
+  setTimeout(function(){ // FIXME this may be a race condition; 
+    $.button.fireEvent('click');
+  }, 200);
 }
 
 $.picker.addEventListener('change',function(e) {
@@ -41,6 +56,7 @@ var xhr = Ti.Network.createHTTPClient({
         } 
         else {
           Ti.API.info("good API call");
+          // FIXME check existence of each element before invoking any methods on it
           $.definition.text = doc.getElementsByTagName("entry").item(0).getAttribute("id") + 
             ": " + doc.getElementsByTagName("pr").item(0).text +
              " " + doc.getElementsByTagName("dt").item(0).text;
@@ -57,26 +73,61 @@ var xhr = Ti.Network.createHTTPClient({
 
 function doClick(e){
     Titanium.API.info("You clicked the button.  Selected = " + selected);
-    var url = "http://www.dictionaryapi.com/api/v1/references/sd2/xml/" 
-              + selected + "?key=7a504103-e56f-4392-96f0-0bf3c6f7eb52";
+    var url = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" 
+              + selected + "?key=6561a4ac-064f-4290-80b2-4101a8085451";
     Ti.API.info(url);
     xhr.open("GET", url);
     xhr.send();  
 };
 
 
+function doCreateLadder(e){
+    Titanium.API.info("You clicked the create button.");
+    //var url = Ti.App.Properties.getString("SERVER_URL");
+    //var n4j_url = "http://localhost:7474/db/data/cypher";
+    var n4j_url = "http://0.0.0.0:3000/words/get_path.json";
+    var start = $.start_word.value.toUpperCase();
+    var target = $.target_word.value.toUpperCase();
+    n4j_url += "?" + "source=" + start.toLowerCase() + "&" + "target=" + target.toLowerCase();
+    
+    if(!start || !target || start.length != target.length) {
+    	alert("Start word and end word must be present and equal length!");
+    	return;
+    }
+    
+    var n4j = Ti.Network.createHTTPClient({
+        onload : function(e) {
+            var dat = JSON.parse( this.responseText );
+            Ti.API.info("dat[0].extracted=" + dat[0].extracted );
+            golden_path = dat[0].extracted;  
+            word = start.split("");
+            $.info.value = target;
+            //initPickerColumns();
+            setPickerRows();
+        },
+        onerror : function(e) {
+            Ti.API.error(e);
+            alert('Error while posting query to neo4j');
+        },
+        timeout : 5000
+    });
+ 
+    n4j.open("GET", n4j_url);
+    n4j.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    Ti.API.info(n4j_url);
+    n4j.send();
+
+}
+
 $.index.addEventListener('open', function() {
-	Ti.API.info("in open event listener.");
-      for(var k=0; k<word.length; k++) {
-      	Ti.API.info("word[k]=" + word[k]);
-      	Ti.API.info("indexOf()=" + alphabet.indexOf( word[k] ));
-      	$.picker.setSelectedRow(k, alphabet.indexOf( word[k] ), false);
-      }
-      setTimeout(function(){ // FIXME this may be a race condition; 
-        $.button.fireEvent('click');
-      }, 200);
-      
+  setPickerRows();      
 });
  
-
+initPickerColumns();
 $.index.open();
+$.info.animate({
+	backgroundColor:'#F00', 
+	color: 'white',
+	duration:3000, 
+	curve:Titanium.UI.ANIMATION_CURVE_EASE_IN_OUT
+});
